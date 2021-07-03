dc782100d0bd21b771465740f323ea40c3882c60
Peter Gates <albeithome@gmail.com>
2021-07-03 13:02:03 -0400

    Update backup_server.sh

--- backup_server.sh
+++ backup_server.sh
@@ -1,10 +1,14 @@
 #! /bin/bash
+
+# make directory if it doesn't exist
+[ -d "output"  ] || mkdir output
+
 # Home\ Videos: the "\ " represents space
 for folder in Backups Documents Downloads Dump Home\ Videos Imports Matt\ Stuff Movies Music Pictures Steven TV
 do
     echo "========================================================="
     echo "======================== $folder ========================"
     echo "========================================================="
-    sudo rsync -havzP --stats /mnt/Server/$folder/* /mnt/e/Server/$folder | tee output.txt
+    sudo rsync -havzP --stats /mnt/Server/$folder/* /mnt/e/Server/$folder | tee output/output_$folder.txt
 done
 
